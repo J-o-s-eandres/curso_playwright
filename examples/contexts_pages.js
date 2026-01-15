6c48c2323533a05996e524e3731ed9ef7e254db6
Joseandres Montesino <Joseandresmontesino@gmail.com>
2026-01-14 22:56:03 -0600

    contexts  pages examples add wikipedia pages

--- examples/contexts_pages.js
+++ examples/contexts_pages.js
@@ -1,3 +1,4 @@
+const { time } = require('node:console');
 const  { chromium } = require('playwright');
 
 (async () => {
@@ -16,6 +17,26 @@ const  { chromium } = require('playwright');
     //wait to see the page
     await googlePage.waitForTimeout(8000);
     console.log('Google page opened');
+
+    //close google context
+    await googleContext.close();
+
+    // Context 2 (wikipedia)
+    const wikipediaContext = await browser.newContext();
+
+    // Create a new page in the context
+    const wikipediaPage = await wikipediaContext.newPage();
+
+    // Navigate to Wikipedia
+    await wikipediaPage.goto('https://www.wikipedia.org');
+
+    //wait to see the page
+    await wikipediaPage.waitForTimeout(8000);
+
+    //close wikipedia context
+    await wikipediaContext.close();
+    console.log('Wikipedia page opened');
+    console.log('Browsers closed');
 
     //close browser
     await browser.close();
